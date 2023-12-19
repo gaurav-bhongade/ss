5993a88eb27e3a881cc6f9fa48b3f2f11b52ce00
Gaurav <gauravbhongade95@gmail.com>
2023-12-20 00:11:11 +0530

    klkl

--- ssf/registration/models.py
+++ ssf/registration/models.py
@@ -5,10 +5,8 @@ class modf(models.Model):
     pass
 class lidfv(models.Model):
     pass
-<<<<<<< HEAD
 
 class pudi(models.Model):
-=======
+    
 class lavda(models.Model):
->>>>>>> temp
     pass
