bd4b07e6a39ed13a12ad38b4f5840fa4edadede6
Gaurav <gauravbhongade95@gmail.com>
2023-12-20 22:49:39 +0530

    add update work done

--- ssf/registration/models.py
+++ ssf/registration/models.py
@@ -1,16 +1,37 @@
 from django.db import models
 
 # Create your models here.
-class modf(models.Model):
-    pass
-class lidfv(models.Model):
-    pass
 
-class pudi(models.Model):
-<<<<<<< HEAD
+class Site(models.Model):
+    name = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+
+    class Meta:
+        db_table = "site_details"
+
+    def __str__(self):
+        return f'''{self.__dict__}'''
     
-=======
-    pass
->>>>>>> e6e1bf02b7c1d35af690d7aef03fe2cb2969d364
-class lavda(models.Model):
-    pass
+    def __repr__(self):
+        return str(self)
+
+class Pipe_Laying(models.Model):
+    label = models.CharField(max_length = 20)
+    start_node = models.CharField(max_length = 20)
+    stop_node = models.CharField(max_length = 20)
+    length = models.IntegerField()
+    diameter_id = models.IntegerField()
+    diameter_od = models.IntegerField()
+    material = models.CharField(max_length = 20)
+    laying_length = models.CharField(max_length = 20, null=True, blank = True)
+    balance_laying = models.IntegerField()
+
+    class Meta:
+        db_table = "pipe_layings"
+
+    def __str__(self):
+        return f'''{self.__dict__}'''
+    
+    def __repr__(self):
+        return str(self)
+    
